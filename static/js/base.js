030cd01f40ab2124e47fdcbfac88c2a7ae7d5b49
zahra <zahra77nikbakhtgmial.com>
2023-03-16 12:55:49 +0330

    something is added

--- static/js/base.js
+++ static/js/base.js
@@ -17,16 +17,23 @@ function goBack(){
     var value = document.referrer.match(/edit/) || document.referrer.match(/discount/);
     if(value){
         history.go(-3);
-        // if(window.location.toString().includes("edit"))
-        // {
-        //     history.go(-2);
-        // }
     }
     else{
         history.go(-1);
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function searchPatient(x) {
